58194faf5424854046cc10c09ddd83ac95634c6b
Helen Quidano <hdquidano@gmail.com>
2024-09-09 08:45:24 -0500

    Add first random iteration

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const randomLetterNumber = ["K", "Q", "J", "1", "2", "3", "4", "5"];
+  const randomSuite = ["heart", "spade"];
+
+  function randomNumberChar() {
+    for (let i = 0; i < randomSuite.length; i++) {
+      for (let j = 0; i < randomLetterNumber.length; j++) {}
+      loopRandom = Math.random(randomSuite);
+      document.getElementById("randomSuite");
+    }
+  }
 };
