433326a6da0502441e8a27712ad11e48369d5d85
Helen Quidano <hdquidano@gmail.com>
2024-09-22 15:11:32 -0500

    Add poker card random generator

--- src/app.js
+++ src/app.js
@@ -8,14 +8,43 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-  const randomLetterNumber = ["K", "Q", "J", "1", "2", "3", "4", "5"];
-  const randomSuite = ["heart", "spade"];
+  const randomLetterNumber = [
+    "K",
+    "Q",
+    "J",
+    "A",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9"
+  ];
+  const randomSuite = ["♦", "♥", "♠", "♣"];
 
   function randomNumberChar() {
-    for (let i = 0; i < randomSuite.length; i++) {
-      for (let j = 0; i < randomLetterNumber.length; j++) {}
-      loopRandom = Math.random(randomSuite);
-      document.getElementById("randomSuite");
-    }
+    let loopRandom = Math.floor(Math.random() * randomLetterNumber.length);
+    let loopRandomSuite = Math.floor(Math.random() * randomSuite.length);
+    return {
+      numberLetter: randomLetterNumber[loopRandom],
+      suite: randomSuite[loopRandomSuite]
+    };
+  }
+  let randomCard = randomNumberChar();
+
+  document.getElementById("suite1").innerText = randomCard.suite;
+  document.getElementById("randomLetter").innerText = randomCard.numberLetter;
+  document.getElementById("suite2").innerText = randomCard.suite;
+
+  if (randomCard.suite === "♦" || randomCard.suite === "♥") {
+    document.getElementById("suite1").style.color = "red";
+    document.getElementById("suite2").style.color = "red";
+  } else {
+    document.getElementById("suite1").style.color = "black";
+    document.getElementById("suite2").style.color = "black";
   }
 };
